866ff3258a8f5046cd37d407168ebe763784721d
Rafael Valverde <rafacvo@gmail.com>
2011-10-23 16:21:05 -0200

    Allow sign_in using email or username.

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,15 +5,22 @@ class User < ActiveRecord::Base
          :recoverable, :rememberable, :trackable, :validatable
   mount_uploader :avatar, AvatarUploader
 
-  # Setup accessible (or protected) attributes for your model
   attr_accessible :username, :email, :password, :password_confirmation, :remember_me,
                   :avatar, :remote_avatar_url
+  attr_accessor :login
 
-  has_many :lists
+  has_many :lists, :dependent => :destroy
   has_many :tasks, :through => :lists
   has_many :unfinished_tasks_due_today, :source => :tasks, :through => :lists, :include => :list,
            :conditions => proc { "finished = false and date(due_on) = '#{Date.today.to_s(:db)}'" },
            :order => :list_id
 
   validates :username, :presence => true, :uniqueness => true
+
+  protected
+  def self.find_for_database_authentication(warden_conditions)
+    conditions = warden_conditions.dup
+    login = conditions.delete(:login)
+    where(conditions).where("lower(username) = :login OR lower(email) = :login", login: login.downcase).first
+  end
 end
